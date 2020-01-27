3efe76a3c211fba1b3dc07ef72a235590de73d4f
neptali04 <monteznepti@outlook.com>
2020-01-27 11:45:56 -0800

    first commit

---
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/footer.php
@@ -0,0 +1,49 @@
+			<!-- footer -->
+			<footer class="footer container-fluid p-0" role="contentinfo">
+				<div class="background" style="background-image: url(<?php echo get_template_directory_uri();?>/img/backgrounds/vader_footer.jpg" alt="Darth Vader");></div>
+				
+					<div class="social d-flex flex-column justify-content-between align-items-center w-100">
+						Follow Star Wars:
+						<ul class="d-flex justify-content-between align-items-center">
+							<li><img src="<?php echo get_template_directory_uri();?>/img/icons/icon_facebook.svg" alt="facebook icon"></li>
+							<li><img src="<?php echo get_template_directory_uri();?>/img/icons/icon_instagram.svg" alt="facebook icon"></li>
+							<li style="width: 11.5px;"><img src="<?php echo get_template_directory_uri();?>/img/icons/icon_tumblr.svg" alt="facebook icon"></li>
+							<li><img src="<?php echo get_template_directory_uri();?>/img/icons/icon_twitter.svg" alt="facebook icon"></li>
+							<li><img src="<?php echo get_template_directory_uri();?>/img/icons/icon_youtube.svg" alt="facebook icon"></li>
+						</ul>
+					</div>
+					<div class="link-bank">
+						<ul class="d-flex flex-column justify-content-between align-items-center">
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+							<li>placeholder for acf repeater</li>
+						</ul>
+					</div>
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+		<!-- analytics -->
+		<script>
+		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+		ga('send', 'pageview');
+		</script>
+
+	</body>
+</html>
---
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/loop.php
@@ -0,0 +1,67 @@
+<?php
+	/* Template Name: Latest Posts
+	   Template Post Type: Post */
+
+	if (have_posts()): while (have_posts()) : the_post(); ?>
+
+<!-- Dynamically adding alt text to variable -->
+		<?php $thumbnail_id  = get_post_thumbnail_id( $post->ID ); ?>
+		<?php $thumbnail_alt = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );?>
+
+	<!-- article -->
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<!-- post thumbnail -->
+		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+
+				<div class="placeholder bottom-line" style="background-image: url(<?php echo the_post_thumbnail_url();?>);" >
+          <img src="<?php echo get_template_directory_uri();?>/img/clear-placeholders/small.png" alt="<?php echo $thumbnail_alt ?>">
+        </div>
+			</a>
+		<?php endif; ?>
+		<!-- /post thumbnail -->
+
+		<div class="padding">
+			<!-- post title -->
+			<h2 class="title">
+				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+			</h2>
+			<!-- /post title -->
+			<div class="text">
+				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+			</div>
+		</div>
+	</article>
+	<!-- /article -->
+		<div class='padding py-0 d-flex flex-row flex-wrap justify-content-between'>
+			<!-- post details -->
+			<div class="col-8">
+				<span class="post-info byline-author "><?php _e( 'By:', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
+
+				<div class="post-info byline-date d-flex">
+					<?php the_category();?>&nbsp// <?php the_time('F j, Y'); ?>
+				</div>
+
+			</div>
+			<!-- /post details -->
+
+			<!-- Code to use the Advanced Custom Field image -->
+			<div class="col-1 align-items-end">
+				<?php if( get_field('profile_picture') ): ?>
+				<img class="profile-picture" src="<?php the_field('profile_picture'); ?>" />
+      		<?php endif; ?>
+			</div>
+		</div>
+
+<?php endwhile; ?>
+
+<?php else: ?>
+
+	<!-- article -->
+	<article>
+		<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+	</article>
+	<!-- /article -->
+
+<?php endif; ?>
